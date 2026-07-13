b5513bc3818f75be4d83a752bb0e7f374bdf6e9d
Pungeun Lee <115451544+20222003@users.noreply.github.com>
2026-07-13 16:45:27 +0900

    [Passed] a로 채워넣기 (2ms, 0MB)

---
+++ trail0/a로 채워넣기/filling-with-a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str1;
+    cin >> str1;
+    str1[1] = 'a';
+    str1[str1.length()-2] = 'a';
+    cout << str1;
+    return 0;
+}
